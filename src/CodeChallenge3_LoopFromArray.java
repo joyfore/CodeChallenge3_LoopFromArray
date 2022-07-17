549ea777a4d55965945b47f98b8b0ca786ab6f52
joice <joydingforever@gmail.com>
2022-07-17 14:47:49 -0700

    Initial commit

---
+++ src/CodeChallenge3_LoopFromArray.java
@@ -0,0 +1,14 @@
+
+class CodeChallenge3_LoopFromArray {
+    public static void main(String[] args) {
+
+    /*
+    Create an array. Print how many indexes (or items) it has.
+    */
+        String[] Y = {":)", ":|", ":(", ">:("};
+        System.out.println(Y[0]);
+        System.out.println(Y[1]);
+        System.out.println(Y[2]);
+        System.out.println(Y[3]);
+    }
+}
